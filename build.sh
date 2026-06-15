1f62f454b8c36dfa5fd56a3e9064ea9e458a78fe
Scillman <Scillman@users.noreply.github.com>
2026-06-15 22:46:52 +0200

    Build destroy old docker container

--- build.sh
+++ build.sh
@@ -1,14 +1,33 @@
 #!/usr/bin/env bash
 
-# Stop and delete old container
-docker stop pipegame
-docker container rm pipegame
+CONTAINER_NAME=pipegame-builder
 
-# Start a new container
+if docker container inspect "$CONTAINER_NAME" >/dev/null 2>&1; then
+    STATUS=$(docker container inspect -f '{{.State.Status}}' "$CONTAINER_NAME")
+    case "$STATUS" in
+        created|running|paused|restarting)
+            echo "Stopping Docker container..."
+            docker stop "$CONTAINER_NAME"
+            echo "Removing Docker container..."
+            docker container rm "$CONTAINER_NAME"
+            ;;
+        exited|dead)
+            echo "Removing Docker container..."
+            docker container rm "$CONTAINER_NAME"
+            ;;
+        *)
+            echo "Container exists but in unsupported of $STATUS"
+            ;;
+    esac
+else
+    echo "Container does not exist"
+fi
+
+echo "Starting Docker container..."
 docker run \
     -v './ai_code:/app' \
-    --name pipegame \
+    --name "$CONTAINER_NAME" \
     pipegame:latest \
-    make pipegame
+    make clean pipegame
 
 # docker logs pipegame
