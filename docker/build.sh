efed57623639a0deb714567f71cbb4bcdbb0c0fc
Scillman <Scillman@users.noreply.github.com>
2026-06-19 23:34:22 +0200

    Now uses docker buildx

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-docker build -t pipegame:latest -f Dockerfile .
+
+CACHE=
+
+if [[ "$1" = "-f" || "$1" = "--force" ]]; then
+    CACHE=--no-cache
+fi
+
+docker buildx build --platform linux/amd64 $CACHE -t pipegame:latest -f Dockerfile .
